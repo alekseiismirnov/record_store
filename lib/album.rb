7d2e077c3e7d9bf5750afcc5d04fa76c45528234
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-30 22:03:12 +0200

    not equal if artists are not

--- lib/album.rb
+++ lib/album.rb
@@ -1,10 +1,11 @@
 class Album
-attr_reader :name
+attr_reader :name, :artist
 
   @@albums = []
 
-  def initialize name
+  def initialize (name = "Noname", artist = "Unknown artist")
     @name = name
+    @artist = artist
   end
 
   def self.all
@@ -12,6 +13,7 @@ attr_reader :name
   end
 
   def == other_album
-    @name == other_album.name
+    @name == other_album.name and
+      @artist == other_album.artist
   end
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -9,5 +9,10 @@ describe '.all' do
   it 'albums with same names are equal' do
     expect(Album.new("Blue") == Album.new("Blue")).to eq(true)
   end
+
+  it 'albusm with different artists are not equal' do
+    expect(Album.new("Black", "Minisinger") == Album.new("Black", "Maxisinger")).to eq(false)
+  end
+
 end
   
