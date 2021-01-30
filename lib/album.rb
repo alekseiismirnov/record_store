22049a0be9c6ba26ed8f78dba6ccf5137b13b269
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-30 21:01:15 +0200

    == compares names

--- lib/album.rb
+++ lib/album.rb
@@ -1,7 +1,17 @@
 class Album
+attr_reader :name
+
   @@albums = []
+
+  def initialize name
+    @name = name
+  end
 
   def self.all
     @@albums 
+  end
+
+  def == other_album
+    @name == other_album.name
   end
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -5,5 +5,9 @@ describe '.all' do
   it 'is empty at the beginning' do
     expect(Album.all).to eq([])
   end
+  
+  it 'albums with same names are equal' do
+    expect(Album.new("Blue") == Album.new("Blue")).to eq(true)
+  end
 end
   
