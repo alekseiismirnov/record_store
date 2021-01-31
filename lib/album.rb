6958b13b933c1f912db34cf871d596f36cc33341
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-01-31 04:34:08 +0200

    save and spec bugfix

--- lib/album.rb
+++ lib/album.rb
@@ -1,16 +1,22 @@
 class Album
-attr_reader :name, :artist, :year
+attr_reader :name, :artist, :year, :id
 
-  @@albums = []
+  @@albums = {} 
+  @@total_rows = 0
 
-  def initialize (name = "Noname", artist = "Unknown artist", year = "")
+  def initialize (name = "Noname", artist = "Unknown artist", year = "", id=nil)
     @name = name
     @artist = artist
-    @year = year.to_i
+    @year = year.to_s
+    @id = id || @@total_rows += 1
   end
 
   def self.all
-    @@albums 
+    @@albums.values
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.artist, self.year, self.id)
   end
 
   def == other_album
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -1,25 +1,39 @@
 require 'rspec'
 require 'album'
 
-describe '.all' do
-  it 'is empty at the beginning' do
-    expect(Album.all).to eq([])
-  end
-  
-  it 'albums with same names are equal' do
-    expect(Album.new("Blue") == Album.new("Blue")).to eq(true)
+describe '#Album' do
+  describe '.all' do
+    it 'is empty at the beginning' do
+      expect(Album.all).to eq([])
+    end
   end
 
-  it 'albusm with different artists are not equal' do
-    expect(Album.new("Black", "Minisinger") == Album.new("Black", "Maxisinger")).to eq(false)
-  end
+  describe '#==' do
+    it 'albums with same names are equal' do
+      expect(Album.new("Blue") == Album.new("Blue")).to eq(true)
+    end
 
-  it 'albums are equal if equal name, artist and year' do
-    expect(Album.new("Black", "Minisinger", 1426) == Album.new("Black", "Minisinger", 1426)).to eq(true)
+    it 'albusm with different artists are not equal' do
+      expect(Album.new("Black", "Minisinger") == Album.new("Black", "Maxisinger")).to eq(false)
+    end
+
+    it 'albums are equal if equal name, artist and year' do
+      expect(Album.new("Black", "Minisinger", 1426) == Album.new("Black", "Minisinger", 1426)).to eq(true)
+    end
   end
 
   it 'year is always string' do
-    expect(Album.new("Black", "Minisinger", 1426).year.class).to eq(Integer)
+    expect(Album.new("Black", "Minisinger", 1426).year.class).to eq(String)
+  end
+
+  describe '#save' do
+    it 'saves albums' do
+      album = Album.new("Black")
+      album2 = Album.new("Blue")
+      album.save
+      album2.save
+      expect(Album.all).to eq([album, album2])
+    end
   end
 end
-  
+
